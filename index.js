fef5486851077b784bd60a8e4b9c15efa255895d
mifthihsan <miftahihsan98@gmail.com>
2020-02-15 15:25:48 -0500

    asdb

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ function handleMessage(sender_psid, received_message) {
     // using which it fills the user table appropriately. 
     nlp.compile( received_message.nlp.entities, userData, received_message );
 
-    console.log( "database = " + dataBase[sender_psid] );
+    console.log( "database = " + dataBase );
 
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API  
