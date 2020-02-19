cbe692be1e68002199ac8559a5c6594888a6bf25
mifthihsan <miftahihsan98@gmail.com>
2020-02-19 12:27:43 -0500

    fixed

--- Information.js
+++ Information.js
@@ -3,113 +3,112 @@ class  Information{
     static flightInformation(origin, destination){
 
 
-        let response = 
-        [
+        let response = [
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 06:00\nDuration 00:45\nPrice : 89.99$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 06:00\nDuration 00:45\nPrice : 89.99$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 08:00\nDuration 00:45\nPrice : 89.99$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 08:00\nDuration 00:45\nPrice : 89.99$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 10:00\nDuration 00:45\nPrice : 99.99$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 10:00\nDuration 00:45\nPrice : 99.99$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 12:00\nDuration 00:45\nPrice : 120.00$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 12:00\nDuration 00:45\nPrice : 120.00$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 14:00\nDuration 00:45\nPrice : 120.00$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 14:00\nDuration 00:45\nPrice : 120.00$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 16:00\nDuration 00:45\nPrice : 120.00$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 16:00\nDuration 00:45\nPrice : 120.00$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 18:00\nDuration 00:45\nPrice : 110.50$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 18:00\nDuration 00:45\nPrice : 110.50$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 20:00\nDuration 00:45\nPrice : 99.99$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 20:00\nDuration 00:45\nPrice : 99.99$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                        type: "postback",
+                        title: "Book Flight!",
+                        payload: "Book Flight",
                     }
                 ],
             },
             {
-                "title": origin + " to " + destination,
-                "subtitle": "Date : " + date + "\nTime : 22:00\nDuration 00:45\nPrice : 99.99$",
-                "image_url": "flight.jpeg",
-                "buttons": [
+                title: origin + " to " + destination,
+                subtitle: "Date : " + date + "\nTime : 22:00\nDuration 00:45\nPrice : 99.99$",
+                image_url: "flight.jpeg",
+                buttons: [
                     {
-                    "type": "postback",
-                    "title": "Book Flight!",
-                    "payload": "Book Flight",
+                    type: "postback",
+                    title: "Book Flight!",
+                    payload: "Book Flight",
                     }
                 ],
             },
