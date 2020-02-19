bb6b024d630f74260088bb3de3d78d13317c7cb1
Nafiz <nafiz6@yahoo.com>
2020-02-19 13:47:14 +0600

    greeting

--- index.js
+++ index.js
@@ -262,6 +262,15 @@ curl -X POST -H "Content-Type: application/json" -d '{
 
             ]
         }
-    ]
+    ],
+    "greeting": [
+    {
+      "locale":"default",
+      "text":"Hello {{user_first_name}}! Book a hotel or a flight!"
+    }, {
+      "locale":"en_US",
+      "text":"Hi {{user_first_name}}! Book a hotel or a flight!"
+    }
+  ]
 }' "https://graph.facebook.com/v6.0/me/messenger_profile?access_token=EAAkdTVETz5UBABiMRU4LChbImzlhRbIZBL76hdxdTZBQCrR8gm3iUlo2MKsdbzQJgYYX6cvdL5KaMrtJueuOwl6pPHZBrZCV3nzdGPL92wFLWnF6GDqISffJMj0SBZAfv07hwo2fqZBdsjw9rwlLkApvuWDWRrZA26K9tNVdsN6hwjZBoBZCFd4GBsb7Px8W5RB4ZD"
  */
