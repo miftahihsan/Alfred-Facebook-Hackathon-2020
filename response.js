8046b9d5a63589018192b80e3cbada486036ff88
mifthihsan <miftahihsan98@gmail.com>
2020-02-19 07:46:59 -0500

    fligt view

--- response.js
+++ response.js
@@ -25,31 +25,30 @@ class Response{
 
     static getFlightView( to, from, date ){
         let response = {
-            "attachment" : {
-                "template_type":"generic",
-                "elements":[
-                    {
-                     "title":"Welcome!",
-                     "image_url":"https://petersfancybrownhats.com/company_image.png",
-                     "subtitle":"We have the right hat for everyone.",
-                     "default_action": {
-                       "type": "web_url",
-                       "url": "https://petersfancybrownhats.com/view?item=103",
-                       "webview_height_ratio": "tall",
-                     },
-                     "buttons":[
-                       {
-                         "type":"web_url",
-                         "url":"https://petersfancybrownhats.com",
-                         "title":"View Website"
-                       },{
-                         "type":"postback",
-                         "title":"Start Chatting",
-                         "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                       }              
-                     ]      
-                   }
-                ]
+            "attachment": {             
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                        {
+                            "title": from + " to " + to,
+                            "subtitle": "Flight date " + date,
+                            "image_url": "https://petersfancybrownhats.com/company_image.png",
+                            "buttons": [
+                                {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                                },
+                                {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                                }
+                            ],
+                        }
+                    ]
+                }
             }
         }
 
