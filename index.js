567b77a14bf68687a142b69fa9940d9ca93b87e6
mifthihsan <miftahihsan98@gmail.com>
2020-02-20 06:41:45 -0500

    Hello adding animation

--- index.js
+++ index.js
@@ -252,8 +252,7 @@ function callSendAPI(sender_psid, response) {
     "recipient": {
       "id": sender_psid
     },
-    "message": response,
-    "sender_action" : "typing_on"
+    "message": response
   }
 
   // Send the HTTP request to the Messenger Platform
