ede74fd42b5b014866fa2419f1e09c0fb0410248
mifthihsan <miftahihsan98@gmail.com>
2020-02-25 06:50:40 -0500

    new adding

--- Dynamo.js
+++ Dynamo.js
@@ -85,7 +85,7 @@ async function getHelper(params){
                     } else {
                         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
                     }
-                });
+                }).promise();
 }
 
 function ifExists(emp_id, table_name){
