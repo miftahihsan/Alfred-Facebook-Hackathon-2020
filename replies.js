1c8da3be0b11ce4b6a50dc91e54a7f67d43d9366
Nafiz <nafiz6@yahoo.com>
2020-02-27 19:41:57 +0600

    Update replies.js

--- replies.js
+++ replies.js
@@ -5,7 +5,7 @@ const replies = {
     Responses.genTextReply("I am an advanced bot designed to be your personal assistant here in the offices of Dunder Mifflin."),
     Responses.genTextReply("I'm also designed to be your very own HR manager. That means you can ask me things you'd normally have to ask your HR department. If the question is too difficult for me I can redirect you to an HR representative for further help."),
     Responses.genTextReply("In order to best serve your needs, I’ve already gathered some basic info about you from the Dunder Mifflin employee records"),
-    Responses.genTextReply("Lets get started! \uD83D\uDE04\n"
+    Responses.genTextReply("Lets get started! \uD83D\uDE04\n")
     ],
   "menu" : Responses.genQuickReply(
       "What do you want to do next? \n" +
