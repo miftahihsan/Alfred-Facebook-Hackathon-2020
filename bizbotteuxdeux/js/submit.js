77573aec33987d405777065b6c26dbd13872065e
Nafiz <nafiz6@yahoo.com>
2020-02-29 01:08:06 +0600

    todolist

---
+++ bizbotteuxdeux/js/submit.js
@@ -0,0 +1,30 @@
+
+
+window.onload = init;
+
+function init() {
+
+}
+
+const button = document.getElementById('post-btn');
+
+button.addEventListener('click', async _ => {
+  try {
+    const response = await fetch('https://getschwifty.herokuapp.com/userList', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:  JSON.stringify({
+        "title" : document.getElementById('title'),
+        "items": {
+          "item1" : document.getElementById('item1')
+        }
+      })
+      }
+    );
+    console.log('Completed!', response);
+  } catch(err) {
+    console.error(`Error: ${err}`);
+  }
+});
