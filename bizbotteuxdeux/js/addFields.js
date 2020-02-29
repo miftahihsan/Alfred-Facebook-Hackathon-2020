8f7a7b6bbf851831a48d360878baaa255fca188e
mifthihsan <miftahihsan98@gmail.com>
2020-02-29 04:46:51 -0500

    added user edit access by reading json from url

--- bizbotteuxdeux/js/addFields.js
+++ bizbotteuxdeux/js/addFields.js
@@ -1,7 +1,7 @@
 
 var i = 1;
 
-function addThings(event){
+function addThings(){
 
         const elem = document.createElement('div');
         elem.setAttribute('class', 'input-field col s12');
@@ -38,6 +38,38 @@ function removeThings(event){
         document.getElementById('dynamicBox'+event).remove();
 }
 
+function addOnPost(value, index){
+
+        // key = key.substring(4, key.length);
+
+        const elem = document.createElement('div');
+        elem.setAttribute('class', 'input-field col s12');
+        elem.setAttribute('id', 'dynamicBox'+index);
+
+        const cross = document.createElement('button');
+        cross.setAttribute('class', 'remove');
+        cross.setAttribute('type', 'button');
+        cross.setAttribute('id', index);
+        cross.setAttribute('onclick', 'removeThings('+index+')')
+
+        const icon = document.createElement('i');
+        icon.setAttribute('class', 'fa fa-window-close');
+        icon.setAttribute('aria-hidden', 'true');
+
+        cross.appendChild(icon);
+
+        const box = document.createElement('input');
+        box.setAttribute('placeholder', 'Add an Item');
+        box.setAttribute('id', 'item'+index);
+        box.setAttribute('type', 'text');
+        box.value = value;
+
+        
+        elem.appendChild(cross);
+        elem.appendChild(box);
+
+        document.getElementById('dynamic').append(elem);
+}
 
 function getItem(){
 
@@ -63,11 +95,6 @@ function getItem(){
                 }
         }
 
-        // console.log(response);
         return response;
 }
 
-
-// <button class = "remove" type="button" > <i class="fa fa-window-close" aria-hidden="true"></i> </button>
-
-// alert('Hello');
--- bizbotteuxdeux/js/submit.js
+++ bizbotteuxdeux/js/submit.js
@@ -3,7 +3,32 @@
 window.onload = init;
 
 function init() {
+  var url_string = decodeURIComponent(window.location.href);
 
+  let arr = url_string.split("=");
+
+  if( arr.length == 1 ){
+    return;
+  }
+
+  url_string = decodeURIComponent(arr[1]);
+
+  var response = JSON.parse(url_string);
+
+  if( response['title'].length > 0 ){
+    document.getElementById('title').value = response['title'];
+  }
+
+  console.log(response['items']);
+  var index = 1000;
+
+  Object.entries(response['items']).forEach(([key, value]) => {
+    if( value.length > 0 ){
+      addOnPost(value, index);
+      index++;
+    }
+    // console.log( key + " " + value);
+  });
 }
 
 const button = document.getElementById('post-btn');
