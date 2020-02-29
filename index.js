99a9f73b5586d3a3b0e663cb31e52e694e2d56c7
miftah <miftahbk619@gmail.com>
2020-02-29 13:37:12 -0500

    adding new replies

--- index.js
+++ index.js
@@ -126,10 +126,10 @@ app.post('/webhook', (req, res) => {
             var text;
             if( !(employee.Item !== undefined && employee.Item !== null) ){
               // NOT in employee check if in public user
-              userData['type'] = "PublicUser";
+              userData['type'] = "Employee"; // change it to plublic user later
 
               if ( !(publicUser.Item !== undefined && publicUser.Item !== null) ){
-                DynamoDB.insert( sender_psid, "PublicUser" );
+                DynamoDB.insert( sender_psid, "Employee" );
                 userData['state'] = "INITIATE";
                 console.log("Done putting the user into the DataBase check for more info, User is an Outsider");
                 text = "Done putting the user into the DataBase check for more info, User is an Outsider";
