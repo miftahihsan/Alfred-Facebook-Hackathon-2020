a7990ade40643e212900af5a1756a14f205908f7
miftah <miftahbk619@gmail.com>
2020-03-01 05:01:03 -0500

    adding new items

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ app.post('/webhook', (req, res) => {
       //  new line
 
       console.log('Sender PSID: ' + sender_psid);
-      console.log('event: ' + webhook_event.entry.messaging[0]);
-      console.log('event: ' + webhook_event.entry.messaging);
+      console.log('event: ' + entry.messaging[0]);
+      console.log('event: ' + entry.messaging);
       
       senderAction(sender_psid, Response.getAnimation("on"));
 
