b969220a51fd035ea49c9d1322457cf95d56033a
miftah <miftahbk619@gmail.com>
2020-03-06 06:47:06 -0500

    adding some chage to line number 425

--- index.js
+++ index.js
@@ -437,7 +437,7 @@ function handleQuickReplies(sender_psid, quick_reply) {
           response = [];
 
           for( var i = 0; i < data.length; i++ ){
-            if( data[i]['set_by'].S == userData['uid'] || data[i]['attendees'].L.contains(userData['uid']) ){
+            if( data[i]['set_by'].S == userData['uid'] ){
               response.concat( Response.genGenericTemplate(data[i]) );
             }
           }
