36e369d38fb0acfd936eb83706b5c1c9b91de40f
miftah <miftahbk619@gmail.com>
2020-03-09 12:55:33 +0600

    trying recursion new msg

--- index.js
+++ index.js
@@ -388,18 +388,22 @@ function seq( sender_psid, response, i ){
   senderAction(sender_psid, Response.getAnimation("on"));
 
   console.log("HERE = " + response[i] + "i = " + i);
-  
-  callSendAPI(sender_psid, response[i])
+  // setTimeout(()=> senderAction( sender_psid, Response.getAnimation("on")), (delay)*1000 + 300 );
+  let serverRes;
+
+  if( !( 'attachment' in response[i] ) ){
+    serverRes = callSendAPI(sender_psid, response[i]);
+  }
+  else{
+    serverRes = setTimeout(()=> callSendAPI(sender_psid, response[i]) , 1500 );
+  }
+
+  serverRes
   .then(res => {
     console.log("SUCEESS " + res);
     
     seq( sender_psid, response, i + 1 );
   })
-  .catch(err => {
-    console.log("ERR " + err);
-
-    seq( sender_psid, response, i + 1 );
-  });
 
 }
 
