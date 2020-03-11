587d76e7346ef5a373674c2e94f9245acbe3581a
Nafiz <nafiz6@yahoo.com>
2020-03-12 05:04:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -183,8 +183,8 @@ app.post('/webhook', (req, res) => {
             }else if (webhook_event.postback) {
               handlePostback(sender_psid, webhook_event.postback);
             }else{
-             // disablePersistentMenu(sender_psid);
-              //sendMessage(sender_psid, Replies.replies["WELCOME_BACK"] );
+              disablePersistentMenu(sender_psid);
+              sendMessage(sender_psid, Replies.replies["WELCOME_BACK"] );
             }
 
             /* 
