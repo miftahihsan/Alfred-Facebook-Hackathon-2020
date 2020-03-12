6c02fe1438811a53765306b5b670278eb2671752
Nafiz <nafiz6@yahoo.com>
2020-03-13 00:33:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/userList', (req, res) => {
     data['title'] = body.title;
     data['items'] = body.items;
 
-    DynamoDB.getUserInfo(body.uid,"Employee", data);
+    DynamoDB.getUserInfo(body.uid,"Employee");
 
     let msg = Response.genQuickReply("Your reminders have been added successfully! ^_^ ", [
       {
