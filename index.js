abc1e8edb032a6f231da20c281ad44b4d8cc0740
Nafiz <nafiz6@yahoo.com>
2020-03-15 01:26:03 +0600

    live chat fix

--- index.js
+++ index.js
@@ -414,7 +414,7 @@ function handleQuickReplies(sender_psid, quick_reply) {
     ));
     userData['state']="ADMINISTRATIVE_TASKS";
   }
-  else if( userData['state'] === 'LIVE_YES' ){
+  else if( userData['state'] === 'LIVE_CHAT' ){
     userData['state'] = "INITIATE";
     sendMessage(sender_psid, response );
     enablePersistentMenu(sender_psid);
