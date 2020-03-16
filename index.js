34d0718d818fc1f8123590b10f755b61eea13cf0
Nafiz <nafiz6@yahoo.com>
2020-03-16 20:58:50 +0600

    remove comment code

--- index.js
+++ index.js
@@ -659,25 +659,6 @@ async function getUserName( sender_psid ){
 
 function sendMessage(sender_psid, responses) {
 
-
-  // if (Array.isArray(responses)) {
-  //   let delay = 0;
-  //   for (let response of responses) {
-
-  //     setTimeout(()=>callSendAPI(sender_psid,response), (delay) * 1000 );   // 0 1000  2000  3000
-  //     setTimeout(()=> senderAction( sender_psid, Response.getAnimation("on")), (delay)*1000 + 300 );   // 300  1300  2300  3300
-
-  //     // it was 2 changed it to 4 
-  //     if ("attachment" in response && response['attachment']['type'] === "image")delay+=4;
-
-  //     delay++;
-  //   }
-  //   setTimeout(()=> senderAction( sender_psid, Response.getAnimation("off")), (delay)*1000 + 300 );   // 300  1300  2300  3300
-
-  // } else {
-  //   callSendAPI(sender_psid, responses);
-  // }
-
   let res = [];
   if ( !Array.isArray(responses)) {
     res.push( responses );
